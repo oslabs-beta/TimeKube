8fd783c38c914f653b65901b9899539a180c4683
rswdch <ryan.sawadichai@outlook.com>
2023-12-02 11:11:09 -0700

    Added comments for view pods

--- src/app/dashboard/CurrentState.jsx
+++ src/app/dashboard/CurrentState.jsx
@@ -7,6 +7,13 @@ const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
 async function getData() {
     try {
+        // 1. Find out all the namespaces that exist
+        //    (this is the same as `kubectl get namespaces`)
+        //    use some k8s api to get all namespaces as an array
+        // 2. Based on the namespaces, find out all the pods that exist
+        //    namespace_array.forEach(k8sApi.listNameSpacedPod(namespace))
+        //    probably return a react component (i.e. card) that displays the pods and basic info
+        // 3. After getting all pods from all namespaces, display this data in a sane way
         const podsRes = await k8sApi.listNamespacedPod('default');
         console.log(podsRes.body);
         return podsRes.body;
