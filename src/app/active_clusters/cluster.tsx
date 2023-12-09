c1362549e9515872d7695db021a3eaeb96b23c60
rswdch <ryan.sawadichai@outlook.com>
2023-12-09 10:52:11 -0700

    Hard link to cluster details

--- src/app/active_clusters/cluster.tsx
+++ src/app/active_clusters/cluster.tsx
@@ -1,24 +1,27 @@
 "use client"
 
 import Link from 'next/link';
-import { MouseEventHandler } from "react";
+import {MouseEventHandler} from "react";
 
 type ClusterProp = {
-    id: string;
-    text: string;
+  id: string;
+  text: string;
 }
 
 export default function Cluster(props: ClusterProp) {
-    const handleViewBtnCLicked = () => {
-        console.log('View btn clicked!');
-    }
-    return (
-        <div className="activeCluster">
-            <div>
-                <button className="btn btn-lg btn-wide text-white font-extrabold bg-gray-800" onClick={handleViewBtnCLicked}>
-                    <Link href={`../cluster_details/?id=${props.id}`}>{props.text}</Link>
-                </button>
-            </div>
-        </div>
-    )
+  const handleViewBtnCLicked = () => {
+    console.log('View btn clicked!');
+  }
+  return (
+    <div className="activeCluster">
+      <div>
+        <Link href={`../dashboard`}>
+          <button className="btn btn-lg btn-wide text-white font-extrabold bg-gray-800" onClick={handleViewBtnCLicked}>
+            {/*<Link href={`../cluster_details/?id=${props.id}`}>{props.text}</Link>*/}
+            <Link href={`../dashboard`}>{props.text}</Link>
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
 }
