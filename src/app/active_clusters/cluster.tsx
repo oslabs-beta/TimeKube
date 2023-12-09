efb1846adb9200a90047f623a83d2dd539c2643c
Bassel <basselmfares@gmail.com>
2023-12-09 12:58:11 -0500

    Added sidebar, navbar, and styling (#7)
    * styling added
    
    * additional styling added
    
    * Hard link to cluster details
    
    ---------
    
    Co-authored-by: rswdch <ryan.sawadichai@outlook.com>

--- src/app/active_clusters/cluster.tsx
+++ src/app/active_clusters/cluster.tsx
@@ -1,28 +1,27 @@
 "use client"
 
 import Link from 'next/link';
-import "../../app/style.css";
-import { MouseEventHandler } from "react";
+import {MouseEventHandler} from "react";
 
 type ClusterProp = {
-    id: string;
-    text: string;
+  id: string;
+  text: string;
 }
 
 export default function Cluster(props: ClusterProp) {
-    const handleViewBtnCLicked = () => {
-        console.log('View btn clicked!');
-    }
-    return (
-        <div className="activeCluster">
-            <div className="clusterInfo">
-                {props.text}
-            </div>
-            <div>
-                <button onClick={handleViewBtnCLicked}>
-                    <Link href={`../cluster_details/?id=${props.id}`}>View</Link>
-                </button>
-            </div>
-        </div>
-    )
+  const handleViewBtnCLicked = () => {
+    console.log('View btn clicked!');
+  }
+  return (
+    <div className="activeCluster">
+      <div>
+        <Link href={`../dashboard`}>
+          <button className="btn btn-lg btn-wide text-white font-extrabold bg-gray-800" onClick={handleViewBtnCLicked}>
+            {/*<Link href={`../cluster_details/?id=${props.id}`}>{props.text}</Link>*/}
+            <Link href={`../dashboard`}>{props.text}</Link>
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
 }
--- src/app/active_clusters/page.tsx
+++ src/app/active_clusters/page.tsx
@@ -5,12 +5,17 @@ import Cluster from './cluster';
 
 const Page: NextPage = () => {
     return (
-      <div className="grid grid-cols-4 gap-4">
+    <div>
+      <div className="drawer-content flex flex-col items-center justify-top text-8xl -mt-36 text-white font-extrabold">User Clusters</div>
+      <div className="grid grid-cols-3 gap-48 mt-36">
         <Cluster id='1' text='Cluster 1'/>
         <Cluster id='2' text='Cluster 2'/>
         <Cluster id='3' text='Cluster 3'/>
         <Cluster id='4' text='Cluster 4'/>
+        <Cluster id='5' text='Cluster 5'/>
+        <Cluster id='6' text='Cluster 6'/>
       </div>
+    </div>
     );
   };
   
