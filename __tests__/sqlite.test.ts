f9d96b09add33fed45b099789d35995615d8acff
rswdch <ryan.sawadichai@outlook.com>
2024-01-06 12:15:39 -0700

    fixed return types of kysely db fns. insert and get implemented

--- __tests__/sqlite.test.ts
+++ __tests__/sqlite.test.ts
@@ -1,38 +1,49 @@
 // import { initDB, insertBackup, getAllBackups } from "@/utils/appdb";
-import { initDB, insertBackup, getAllBackups, deleteBackupById, getBackup } from "@/utils/appdb-kysely";
+import {
+  initDB,
+  insertBackup,
+  getAllBackups,
+  deleteBackupById,
+  getBackup,
+} from "@/utils/appdb-kysely";
 
-describe('sqlite database tests', () => {
+describe("sqlite database tests", () => {
   let insertedId: number | null;
   // let insertedId: any;
 
   beforeAll(async () => {
-    insertedId = -1;
+    insertedId = null;
     initDB();
   });
 
-  xit('should get all rows', async () => {
-    insertedId = await insertBackup('default', 'https://www.get-test.com');
+  it("should get all rows", async () => {
+    // need to insert in case db is empty
+    // need to assign insertedId for cleanup
+    const newRow = await insertBackup("default", "https://www.get-test.com");
+    insertedId = newRow.id;
+    if (insertedId === null) {
+      throw new Error("Failure to insert: id is null");
+    }
+
     const rows = await getAllBackups();
-    console.log(rows);
     expect(rows.length).toBeGreaterThan(0);
-  })
+  });
 
-  it('should get a backup by id', async () => {
-    insertedId = await insertBackup('default', 'https://www.get-id-test.com');
-    console.log(insertedId)
+  it("should get a backup by id", async () => {
+    const newRow = await insertBackup("default", "https://www.get-id-test.com");
+    insertedId = newRow.id;
     const inserted = await getBackup(insertedId);
-    expect(inserted.url).toBe('https://www.get-id-test.com');
-  })
+    expect(inserted.url).toBe("https://www.get-id-test.com");
+  });
 
-  xit('should insert a url', async () => {
-    insertedId = await insertBackup('default', 'https://www.insert-test.com');
-    const inserted = await getBackup(insertedId);
+  it("should insert a url", async () => {
+    const insertedRow = await insertBackup("default", "https://www.insert-test.com");
+    insertedId = insertedRow.id;
   });
 
   afterEach(async () => {
     if (insertedId) {
       await deleteBackupById(insertedId);
     }
-  })
-
-})
+  });
+});
--- src/utils/appdb-kysely.ts
+++ src/utils/appdb-kysely.ts
@@ -1,39 +1,45 @@
-import { DB } from 'kysely-codegen' // this is the Database interface we defined earlier
-import SQLite from 'better-sqlite3'
-import { Kysely, SqliteDialect } from 'kysely'
-import { initDB } from './appdb'
+import { DB } from "kysely-codegen"; // this is the Database interface we defined earlier
+import SQLite from "better-sqlite3";
+import { Kysely, SqliteDialect } from "kysely";
+import { initDB } from "./appdb";
 
 const dialect = new SqliteDialect({
-  database: new SQLite('appdata/snapshots.db'),
-})
+  database: new SQLite("appdata/snapshots.db"),
+});
 
 export const db = new Kysely<DB>({
   dialect,
-})
+});
 
 export async function insertBackup(clusterId: string, url: string) {
-  const insertedId = await db.insertInto('backups').values({clusterId, url}).returning(['id']).executeTakeFirstOrThrow();
-  console.log('Sample entry inserted with ID:', insertedId);
-
-  return insertedId;
+  const insertedRow = await db
+    .insertInto("backups")
+    .values({ clusterId, url })
+    .returningAll()
+    .executeTakeFirstOrThrow();
+  return insertedRow;
 }
 
 export async function getBackup(id: number | null) {
   if (!id) {
-    throw new Error('id is null');
+    throw new Error("id is null");
   }
-  const res = await db.selectFrom('backups').select('url').where('id', '=', id).execute();
-  console.log(res)
+  const res = await db
+    .selectFrom("backups")
+    .selectAll()
+    .where("id", "=", id)
+    .executeTakeFirstOrThrow();
+  console.log(res);
   return res;
 }
 
 export async function getAllBackups() {
-  return await db.selectFrom('backups').selectAll().execute();
+  return await db.selectFrom("backups").selectAll().execute();
 }
 
 export async function deleteBackupById(id: number | null) {
   if (!id) return;
-  return await db.deleteFrom('backups').where('id', '=', id).execute();
+  return await db.deleteFrom("backups").where("id", "=", id).execute();
 }
 
-export { initDB }
+export { initDB };
