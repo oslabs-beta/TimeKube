7cf349c0fbe776e2305c2b827323e74aded2747f
rswdch <ryan.sawadichai@outlook.com>
2024-01-16 22:02:02 -0700

    WIP backup handler error sql database

--- src/app/backup/backup.action.ts
+++ src/app/backup/backup.action.ts
@@ -3,12 +3,27 @@ import { uploadToS3 } from "@/services/aws-s3-service";
 import { insertBackup } from "@/utils/appdb-kysely";
 import { saveClusterToSingleYaml, saveClusterToYaml } from "@/utils/backup";
 
-export async function backupHandler() {
+/**
+ * Event handler to back up cluster to yaml and upload to s3, recording s3 URL in database
+ * @param {string} [clusterId="default"] - ID of target cluster to back up
+ */
+export async function backupHandler(clusterId = "default") {
   // saveClusterToYaml();
-  const localSavedFilePath = saveClusterToSingleYaml();
-  
+  // save cluster state to a single yaml file
+  const localSavedFilePath = await saveClusterToSingleYaml();
+  if (typeof localSavedFilePath !== 'string'){
+    throw new Error("YamlSaveError")
+  }
+
+  // backup file to AWS
+  await backupFile(localSavedFilePath, clusterId);
 }
 
+/**
+ * Saves a file specified by a file path to AWS.
+ * @param {string} filePath - path to YAML backup of cluster
+ * @param {string} [clusterId="default"] - ID of cluster being backed up
+ */
 export async function backupFile(filePath: string, clusterId = "default") {
   // Back up to s3
   const s3data = await uploadToS3(filePath, clusterId);
