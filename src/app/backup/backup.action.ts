5aa7448b8bd832578fb48c097cb046e224848e67
rswdch <ryan.sawadichai@outlook.com>
2024-01-17 20:55:03 -0700

    Refactored backup handler to handle FormData

--- src/app/backup/backup.action.ts
+++ src/app/backup/backup.action.ts
@@ -1,4 +1,4 @@
-'use server'
+"use server";
 import { uploadToS3 } from "@/services/aws-s3-service";
 import { insertBackup } from "@/utils/appdb-kysely";
 import { saveClusterToSingleYaml, saveClusterToYaml } from "@/utils/backup";
@@ -7,12 +7,14 @@ import { saveClusterToSingleYaml, saveClusterToYaml } from "@/utils/backup";
  * Event handler to back up cluster to yaml and upload to s3, recording s3 URL in database
  * @param {string} [clusterId="default"] - ID of target cluster to back up
  */
-export async function backupHandler(clusterId = "default") {
+export async function backupHandler(formData: FormData) {
   // saveClusterToYaml();
   // save cluster state to a single yaml file
+  const clusterId = formData.get("clusterId") as string;
+  console.log("ClusterId from backupHandler: ", clusterId);
   const localSavedFilePath = await saveClusterToSingleYaml();
-  if (typeof localSavedFilePath !== 'string'){
-    throw new Error("YamlSaveError")
+  if (typeof localSavedFilePath !== "string") {
+    throw new Error("YamlSaveError");
   }
 
   // backup file to AWS
@@ -26,11 +28,19 @@ export async function backupHandler(clusterId = "default") {
  */
 export async function backupFile(filePath: string, clusterId = "default") {
   // Back up to s3
-  const s3data = await uploadToS3(filePath, clusterId);
-  const s3url = s3data.Location;
-  const s3key = s3data.Key;
-
-  // Record info in db
-  const dbdata = await insertBackup(s3url, s3key, clusterId);
-  return dbdata;
+  try {
+    const s3data = await uploadToS3(filePath, clusterId);
+    const s3url = s3data.Location;
+    const s3key = s3data.Key;
+    console.log("s3 key: ",s3data.Key)
+    if (s3data.Location === undefined || s3data.Key === undefined) {
+      console.error("URL", s3data.Location, "Key", s3data.Key)
+      throw new Error("S3UploadError");
+    }
+    // Record info in db
+    const dbdata = await insertBackup(s3url, s3key, clusterId);
+    return dbdata;
+  } catch (e) {
+    console.log("Error in backupFile: ", e);
+  }
 }
