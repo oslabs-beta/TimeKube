38ec165be7937f50c4dd9a3b266536fa1ed0d8f8
rswdch <ryan.sawadichai@outlook.com>
2024-01-17 20:55:37 -0700

    Button sends clusterId as FormData

--- src/app/backup/BackupButton.tsx
+++ src/app/backup/BackupButton.tsx
@@ -1,8 +1,14 @@
+'use client'
+import { useSearchParams } from 'next/navigation'
 import { backupHandler } from "./backup.action";
 
 export default async function BackupButton({text}: {text: string}) {
+  const searchParams = useSearchParams()
+  const clusterId = searchParams.get('clusterId')
+  console.log('clusterId in BackupButton', clusterId)
   return (
     <form action={backupHandler}>
+      <input type="hidden" name="clusterId" value={clusterId ? clusterId : "default"}/>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         type="submit"
