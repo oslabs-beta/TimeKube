8a50a2fe4a9dc79cd222895b29db7b7db315e6ae
rswdch <ryan.sawadichai@outlook.com>
2024-01-23 21:12:06 -0700

    Removed flex from sidebar contents (page contents)

--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -10,7 +10,8 @@ export default function Sidebar({ content }: { content: React.ReactNode }) {
     return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
+      {/* <div className="drawer-content flex flex-col items-center justify-center"> */}
+      <div className="drawer-content">
         {content}
         <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
         
