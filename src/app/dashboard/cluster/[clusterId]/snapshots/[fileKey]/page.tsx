110b578ac0908f39153b411b482cc82b9518ed67
rswdch <ryan.sawadichai@outlook.com>
2024-02-12 19:43:42 -0700

    Cache backup file from s3

--- src/app/dashboard/cluster/[clusterId]/snapshots/[fileKey]/page.tsx
+++ src/app/dashboard/cluster/[clusterId]/snapshots/[fileKey]/page.tsx
@@ -1,9 +1,15 @@
 import {downloadFromS3} from "@/services/aws-s3-service.ts";
 import "highlight.js/styles/atom-one-dark.css";
 import YamlDisplay from "@/app/dashboard/cluster/[clusterId]/snapshots/[fileKey]/YamlDisplay.tsx";
+import { cache } from "react";
 
+export const revalidate = 3600 // revalidate at most every hour
 export default async function SnapshotFilePage({params}) {
-  const fileString = await downloadFromS3(params.fileKey);
+  // The React cache function is used to memoize data requests.
+  const getFileString = cache(async (fileKey: string) => {
+    return await downloadFromS3(fileKey)
+  });
+  const fileString = await getFileString(params.fileKey);
   return (
     <div>
       <YamlDisplay fileString={fileString}/>
