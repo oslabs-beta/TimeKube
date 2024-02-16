4804321c4685de59c15e28e02749f250ac501d72
rswdch <ryan.sawadichai@outlook.com>
2024-02-15 19:15:58 -0700

    Changed anchors to links to enable loading animation

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image"
+import Link from "next/link"
 export default function Navbar() {
   return (
     <nav className="bg-white border-b-2 border-gray-100">
       <div className="mx-16 px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-28 items-center justify-between">
           <div className="flex flex-1 items-center justify-center lg:items-stretch lg:justify-start">
-            <a href="/" className="flex flex-shrink-0 items-center">
+            <Link href="/" className="flex flex-shrink-0 items-center">
               <Image className="h-12 w-auto rounded-full" src="/TimeKubeLogo.png" alt="TimeKube Logo" width={64} height={64}/>
-            </a>
+            </Link>
             <div className="ml-6">
               <div className="flex space-x-10">
                 <a href="/" className="bg-gray-900 text-white rounded-md px-3 py-2 text-2xl font-medium" aria-current="page">TimeKube</a>
@@ -19,9 +20,9 @@ export default function Navbar() {
           </div>
             <div className="hidden lg:ml-6 lg:block">
               <div className="flex space-x-10">
-                <a href="/dashboard/cluster" className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">Clusters</a>
-                <a href="/dashboard/snapshots" className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">Snapshots</a>
-                <a href="/dashboard/metrics" className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">Metrics</a>
+                <Link href="/dashboard/cluster" className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">Clusters</Link>
+                <Link href="/dashboard/snapshots" className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">Snapshots</Link>
+                <Link href="/dashboard/metrics" className="text-gray-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-xl font-medium">Metrics</Link>
               </div>
             </div>
         </div>
@@ -29,9 +30,9 @@ export default function Navbar() {
 
       <div className="lg:hidden" id="mobile-menu">
         <div className="space-y-1 px-2 pb-3 pt-2">
-          <a href="/dashboard/cluster" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Clusters</a>
-          <a href="/dashboard/snapshots" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Snapshots</a>
-          <a href="/dashboard/metrics" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Metrics</a>
+          <Link href="/dashboard/cluster" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Clusters</Link>
+          <Link href="/dashboard/snapshots" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Snapshots</Link>
+          <Link href="/dashboard/metrics" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Metrics</Link>
         </div>
       </div>
     </nav>
